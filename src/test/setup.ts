f4fbdacfaa71cb66a0a15a424f9340f982e2de18
ammar-kashif <ammarkashif00@gmail.com>
2026-03-12 01:54:01 +0500

    feat: initial commit

---
+++ src/test/setup.ts
@@ -0,0 +1,18 @@
+import '@testing-library/jest-dom';
+import 'fake-indexeddb/auto';
+import { setupServer } from 'msw/node';
+import { afterAll, afterEach, beforeAll } from 'vitest';
+
+export const server = setupServer();
+
+beforeAll(() => {
+  server.listen({ onUnhandledRequest: 'bypass' });
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
